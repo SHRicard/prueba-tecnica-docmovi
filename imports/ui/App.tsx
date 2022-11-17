1f14a49809e0e631876feac21d53aa15b616fcba
Ricardo <SH_Ricardo@hotmail.com>
2022-11-16 21:07:50 -0300

    imports component and chage navbar

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,25 +1,32 @@
 import React from "react";
-
 import Form from "/imports/ui/Components/Form";
-
 import Tabla from "/imports/ui/Components/Tabla";
+
 const App = () => {
   return (
-    <div className="container-fluid ">
-      <div className="row">
-        <div className="col-12 pt-3 pb-3 text-center bg-info ">
-          <small className="fs-3 fw-bold  text-uppercase">
-            Prueba Tecnica Meteor | TypeScript | Tailwind
-          </small>
-        </div>
-        <div className="col-12 pt-4 text-center">
-          <small className="fs-5 fw-bold text-uppercase  ">
-            Tabla de Pacientes
-          </small>
-          <Tabla />
-        </div>
-        <div className="col-12">
+    <div className="container-fluid bg-light ">
+      <div className="row d-flex justify-content-center">
+        <nav className="navbar navbar-dark bg-secondary">
+          <div className="container-fluid">
+            <a className="navbar-brand px-3" href="#">
+              Prueba Tecnica
+            </a>
+          </div>
+        </nav>
+
+        <div className="col-12 col-md-10 my-5 border">
+          <h2 className="fs-4 my-3 fw-bold text-center text-uppercase">
+            Crear Paciente
+          </h2>
           <Form />
+        </div>
+
+        <div className="col-12 col-md-10 mx-0 px-0 pt-2 border ">
+          <h4 className=" fw-bold text-uppercase text-center py-3">
+            Lista de Pacientes
+          </h4>
+
+          <Tabla />
         </div>
       </div>
     </div>
